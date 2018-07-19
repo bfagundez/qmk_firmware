ece2f4cb1f6a515f7a09b2918950b43bbd77992c
Bruno Fagundez <bfagundez@gmail.com>
2018-07-19 16:42:47 -0700

    Added vim new tab shortcut to the adjust layer

--- keyboards/minidox/keymaps/geekymartian/keymap.c
+++ keyboards/minidox/keymaps/geekymartian/keymap.c
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------.           ,----------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |           |   F6 |  F7  |  F8  |  F9  |  F10 |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  F11 |  F12 |      | Mute |VolUP |           |ViPanL|ViPanD|ViPanU|ViPanR|      |
+ * |  F11 |  F12 |      | Mute |VolUP |           |ViPanL|ViPanD|ViPanU|ViPanR|viNtab|
  * |------+------+------+------+------|           |------+------+------+------+------|
  * | Reset|      |      |      |VolDwn|           |TxPanL|TxPanD|TxPanU|TxPanR|      |
  * `----------------------------------'           `----------------------------------'
@@ -123,7 +123,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] =  LAYOUT( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,    KC_F5,        KC_F6,          KC_F7,          KC_F8,        KC_F9,           KC_F10, \
-  KC_F11,  KC_F12,  _______, KC__MUTE, KC__VOLUP,    VIM_PANE_LEFT,  VIM_PANE_DOWN,  VIM_PANE_UP,  VIM_PANE_RIGHT,  _______ , \
+  KC_F11,  KC_F12,  _______, KC__MUTE, KC__VOLUP,    VIM_PANE_LEFT,  VIM_PANE_DOWN,  VIM_PANE_UP,  VIM_PANE_RIGHT,  VIM_NEW_TAB , \
   RESET,   _______, _______, _______,  KC__VOLDOWN,  TMUX_PANE_LEFT, TMUX_PANE_DOWN, TMUX_PANE_UP, TMUX_PANE_RIGHT, _______, \
                     _______, _______, _______,       _______,  _______, _______                    \
 )
