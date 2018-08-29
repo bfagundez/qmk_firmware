b0a5a1d5c45a01b21852ea0b4bcf02d92528a4f0
Bruno Fagundez <bfagundez@gmail.com>
2018-08-29 10:09:30 -0700

    Added DVORAK

--- keyboards/minidox/keymaps/geekymartian/keymap.c
+++ keyboards/minidox/keymaps/geekymartian/keymap.c
@@ -9,13 +9,15 @@ extern keymap_config_t keymap_config;
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
 #define _QWERTY 0
-#define _LOWER 1
-#define _RAISE 2
+#define _DVORAK 1
+#define _LOWER 2
+#define _RAISE 3
 #define _ADJUST 16
 
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
+  DVORAK,
   LOWER,
   RAISE,
   ADJUST,
@@ -53,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,----------------------------------.           ,----------------------------------.
  * |   Q  |   W  |   E  |   R  |   T  |           |   Y  |   U  |   I  |   O  |   P  |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |   A  |   S  |   D  |   F  |   G  |           |   H  |   J  |   K  |   L  |Shft/;|
+ * |SFT/A |   S  |   D  |   F  |   G  |           |   H  |   J  |   K  |   L  |Shft/;|
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |ctl/Z |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |alt /.|Ctl / |
  * `----------------------------------'           `----------------------------------'
@@ -65,10 +67,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT( \
   KC_Q,        KC_W, KC_E, KC_R, KC_T,              KC_Y, KC_U, KC_I,    KC_O,          KC_P,    \
-  KC_A,        KC_S, KC_D, KC_F, KC_G,              KC_H, KC_J, KC_K,    KC_L,          SFT_T(KC_SCLN), \
+  SFT_T(KC_A), KC_S, KC_D, KC_F, KC_G,              KC_H, KC_J, KC_K,    KC_L,          SFT_T(KC_SCLN), \
   CTL_T(KC_Z), KC_X, KC_C, KC_V, KC_B,              KC_N, KC_M, KC_COMM, ALT_T(KC_DOT), CTL_T(KC_SLSH), \
                KC_LGUI, KC_SPC, LOWER,              RAISE, KC_ENT, KC_TAB                \
 ),
+
+/* Dvorak
+ *
+ * ,----------------------------------.           ,----------------------------------.
+ * |   '  |   ,  |   .  |   P  |   Y  |           |   F  |   G  |   C  |   R  |   L  |
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |SFT/A |   O  |   E  |   U  |   I  |           |   D  |   H  |   T  |   N  |SFT/ S|
+ * |------+------+------+------+------|           |------+------+------+------+------|
+ * |CTL/ ;|   Q  |   J  |   K  |   X  |           |   B  |   M  |   W  | ALT/V|CTL/ Z|
+ * `----------------------------------'           `----------------------------------'
+ *                  ,--------------------.    ,------,-------------.
+ *                  | GUI | Space |LOWER |    |RAISE |Enter | Tab  |
+ *                  `-------------|      |    |      |------+------.
+ *                                |      |    |      |
+ *                                `------'    `------'
+ */
+[_DVORAK] = LAYOUT ( \
+  KC_QUOT,        KC_COMM, KC_DOT, KC_P, KC_Y,       KC_F,    KC_G,    KC_C,    KC_R,        KC_L,    \
+  SFT_T(KC_A),    KC_O,    KC_E,   KC_U, KC_I,       KC_D,    KC_H,    KC_T,    KC_N,        SFT_T(KC_S),   \
+  CTL_T(KC_SCLN), KC_Q,    KC_J,   KC_K, KC_X,       KC_B,    KC_M,    KC_W,    ALT_T(KC_V), CTL_T(KC_Z),  \
+                      KC_LGUI, KC_SPC,  LOWER,       RAISE,   KC_ENT,  KC_TAB                \
+),
+
 /* Raise
  *
  * ,------------------------------------.        ,----------------------------------.
@@ -131,7 +156,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_ADJUST] =  LAYOUT( \
   KC_F1,   KC_F2,   KC_F3,    KC_F4,       KC_F5,        KC_F6,    KC_F7,   KC_F8,   KC_F9,    KC_F10,  \
   KC_F11,  KC_F12,  KC__MUTE, KC__VOLDOWN, KC__VOLUP,    KC_MS_L,  KC_MS_D, KC_MS_U, KC_MS_R,  KC_WH_U, \
-  KC_BTN1, KC_BTN2, _______,  _______,     _______,      RGB_TOG,  RGB_MOD, RGB_VAI, RGB_VAD,  KC_WH_D, \
+  KC_BTN1, KC_BTN2, _______,  QWERTY,      DVORAK,       RGB_TOG,  RGB_MOD, RGB_VAI, RGB_VAD,  KC_WH_D, \
                     _______,  _______,     _______,      _______,  _______, _______                     \
 )
 };
@@ -143,9 +168,16 @@ void persistant_default_layer_set(uint16_t default_layer) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    // Keyboard Layers
     case QWERTY:
       if (record->event.pressed) {
         persistant_default_layer_set(1UL<<_QWERTY);
+      }
+      return false;
+      break;
+    case DVORAK:
+      if (record->event.pressed) {
+        persistant_default_layer_set(1UL<<_DVORAK);
       }
       return false;
       break;
