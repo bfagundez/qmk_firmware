812b7cfdf909299eaba328e0272bac044152a4ea
Bruno Fagundez <bfagundez@gmail.com>
2019-01-30 22:01:34 -0800

    Optimizing layer change

--- keyboards/minidox/keymaps/geekymartian/keymap.c
+++ keyboards/minidox/keymaps/geekymartian/keymap.c
@@ -20,7 +20,6 @@ enum custom_keycodes {
   LOWER,
   FANCY,
   RAISE,
-  EXTRA,
   ADJUST,
   VIM_PASTE_LAST_REGISTER,
   VIM_EQUALIZE_PANES,
@@ -79,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,        KC_W, KC_E, KC_R, KC_T,              KC_Y, KC_U, KC_I,    KC_O,          KC_P,    \
   SFT_T(KC_A), KC_S, KC_D, KC_F, KC_G,              KC_H, KC_J, KC_K,    KC_L,          SFT_T(KC_SCLN), \
   CTL_T(KC_Z), KC_X, KC_C, KC_V, KC_B,              KC_N, KC_M, KC_COMM, ALT_T(KC_DOT), CTL_T(KC_SLSH), \
-               KC_LGUI, KC_SPC, LOWER,              RAISE, KC_ENT, EXTRA                \
+               KC_LGUI, KC_SPC, LOWER,              RAISE, KC_ENT, MO(_EXTRA)                \
 ),
 /* Lower (Macro full)
  *
@@ -219,14 +218,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       } else {
         layer_off(_RAISE);
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
-      }
-      return false;
-      break;
-    case EXTRA:
-      if (record->event.pressed) {
-        layer_on(_EXTRA);
-      } else {
-        layer_off(_EXTRA);
       }
       return false;
       break;
