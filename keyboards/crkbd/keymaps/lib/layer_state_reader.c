41c3804f4948a707d27a3866b02a58b62c8eceff
Bruno Fagundez <bfagundez@gmail.com>
2019-12-05 16:37:22 -0800

    Fixed layer states

--- keyboards/crkbd/keymaps/lib/layer_state_reader.c
+++ keyboards/crkbd/keymaps/lib/layer_state_reader.c
@@ -1,11 +1,10 @@
 #include "crkbd.h"
 
 #define L_BASE 0
-#define L_LOWER 2
-#define L_RAISE 3
-#define L_EXTRA 4
-#define L_ADJUST 16
-#define L_ADJUST_TRI 65560
+#define L_LOWER 4
+#define L_RAISE 8
+#define L_EXTRA 16
+#define L_ADJUST 65548
 
 char layer_state_str[40];
 
@@ -14,23 +13,22 @@ char *read_layer_state(void)
   switch (layer_state)
   {
     case L_BASE:
-       snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
-       break;
-    case L_RAISE:
-       snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+       snprintf(layer_state_str, sizeof(layer_state_str), "<< QWERTY >>");
        break;
     case L_LOWER:
-       snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
+       snprintf(layer_state_str, sizeof(layer_state_str), "<< LOWER >>");
        break;
-    case L_ADJUST:
-    case L_ADJUST_TRI:
-       snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Adjust");
+    case L_RAISE:
+       snprintf(layer_state_str, sizeof(layer_state_str), "<< RAISE >>");
        break;
     case L_EXTRA:
-       snprintf(layer_state_str, sizeof(layer_state_str), "Layer: EXTRA!!!");
+       snprintf(layer_state_str, sizeof(layer_state_str), "<< EXTRA >>");
+       break;
+    case L_ADJUST:
+       snprintf(layer_state_str, sizeof(layer_state_str), "<< ADJUST >>");
        break;
     default:
-       snprintf(layer_state_str,sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
+       snprintf(layer_state_str,sizeof(layer_state_str), "WTH? ==> %ld", layer_state);
   }
 
   return layer_state_str;
